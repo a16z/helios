2e71664911647ce7ee166a80c95a34f22208f9e4
Noah Citron <noah@jeff.org>
2022-09-09 17:52:44 -0400

    add checkpoint flag

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use std::{sync::Arc, time::Duration};
 
 use clap::Parser;
+use common::utils::hex_str_to_bytes;
 use dirs::home_dir;
 use env_logger::Env;
 use eyre::Result;
@@ -14,7 +15,7 @@ async fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let cli = Cli::parse();
-    let config = match cli.network.as_str() {
+    let mut config = match cli.network.as_str() {
         "goerli" => networks::goerli(),
         _ => {
             let home = home_dir().unwrap();
@@ -22,6 +23,10 @@ async fn main() -> Result<()> {
             Config::from_file(&config_path).unwrap()
         }
     };
+
+    if let Some(checkpoint) = cli.checkpoint {
+        config.general.checkpoint = hex_str_to_bytes(&checkpoint)?;
+    }
 
     let mut client = Client::new(Arc::new(config)).await?;
     client.sync().await?;
@@ -39,8 +44,10 @@ async fn main() -> Result<()> {
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(long, default_value = "goerli")]
+    #[clap(short, long, default_value = "goerli")]
     network: String,
-    #[clap(long, default_value = "8545")]
+    #[clap(short, long, default_value = "8545")]
     port: u16,
+    #[clap(short, long)]
+    checkpoint: Option<String>,
 }
