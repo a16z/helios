3573a31dbc91ba28b9aee8df28cfcbde33f17bc8
Noah Citron <noah@jeff.org>
2022-09-22 16:03:06 -0400

    feat: use RwLock for concurrent reads

--- client/src/client.rs
+++ client/src/client.rs
@@ -10,7 +10,7 @@ use consensus::types::Header;
 use execution::types::{CallOpts, ExecutionBlock};
 use log::{info, warn};
 use tokio::spawn;
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 use tokio::time::sleep;
 
 use crate::database::{Database, FileDB};
@@ -18,7 +18,7 @@ use crate::node::{BlockTag, Node};
 use crate::rpc::Rpc;
 
 pub struct Client<DB: Database> {
-    node: Arc<Mutex<Node>>,
+    node: Arc<RwLock<Node>>,
     rpc: Option<Rpc>,
     db: DB,
 }
@@ -27,7 +27,7 @@ impl Client<FileDB> {
     pub async fn new(config: Config) -> Result<Self> {
         let config = Arc::new(config);
         let node = Node::new(config.clone()).await?;
-        let node = Arc::new(Mutex::new(node));
+        let node = Arc::new(RwLock::new(node));
 
         let rpc = if let Some(port) = config.general.rpc_port {
             Some(Rpc::new(node.clone(), port))
@@ -48,13 +48,13 @@ impl<DB: Database> Client<DB> {
 
         let node = self.node.clone();
         spawn(async move {
-            let res = node.lock().await.sync().await;
+            let res = node.write().await.sync().await;
             if let Err(err) = res {
                 warn!("{}", err);
             }
 
             loop {
-                let res = node.lock().await.advance().await;
+                let res = node.write().await.advance().await;
                 if let Err(err) = res {
                     warn!("{}", err);
                 }
@@ -70,7 +70,7 @@ impl<DB: Database> Client<DB> {
         println!();
         info!("shutting down");
 
-        let node = self.node.lock().await;
+        let node = self.node.write().await;
         let checkpoint = if let Some(checkpoint) = node.get_last_checkpoint() {
             checkpoint
         } else {
@@ -85,31 +85,31 @@ impl<DB: Database> Client<DB> {
     }
 
     pub async fn call(&self, opts: &CallOpts, block: &BlockTag) -> Result<Vec<u8>> {
-        self.node.lock().await.call(opts, block)
+        self.node.write().await.call(opts, block)
     }
 
     pub async fn estimate_gas(&self, opts: &CallOpts) -> Result<u64> {
-        self.node.lock().await.estimate_gas(opts)
+        self.node.write().await.estimate_gas(opts)
     }
 
     pub async fn get_balance(&self, address: &Address, block: &BlockTag) -> Result<U256> {
-        self.node.lock().await.get_balance(address, block).await
+        self.node.write().await.get_balance(address, block).await
     }
 
     pub async fn get_nonce(&self, address: &Address, block: &BlockTag) -> Result<u64> {
-        self.node.lock().await.get_nonce(address, block).await
+        self.node.write().await.get_nonce(address, block).await
     }
 
     pub async fn get_code(&self, address: &Address, block: &BlockTag) -> Result<Vec<u8>> {
-        self.node.lock().await.get_code(address, block).await
+        self.node.write().await.get_code(address, block).await
     }
 
     pub async fn get_storage_at(&self, address: &Address, slot: H256) -> Result<U256> {
-        self.node.lock().await.get_storage_at(address, slot).await
+        self.node.write().await.get_storage_at(address, slot).await
     }
 
     pub async fn send_raw_transaction(&self, bytes: &Vec<u8>) -> Result<H256> {
-        self.node.lock().await.send_raw_transaction(bytes).await
+        self.node.write().await.send_raw_transaction(bytes).await
     }
 
     pub async fn get_transaction_receipt(
@@ -117,7 +117,7 @@ impl<DB: Database> Client<DB> {
         tx_hash: &H256,
     ) -> Result<Option<TransactionReceipt>> {
         self.node
-            .lock()
+            .write()
             .await
             .get_transaction_receipt(tx_hash)
             .await
@@ -125,37 +125,37 @@ impl<DB: Database> Client<DB> {
 
     pub async fn get_transaction_by_hash(&self, tx_hash: &H256) -> Result<Option<Transaction>> {
         self.node
-            .lock()
+            .write()
             .await
             .get_transaction_by_hash(tx_hash)
             .await
     }
 
     pub async fn get_gas_price(&self) -> Result<U256> {
-        self.node.lock().await.get_gas_price()
+        self.node.write().await.get_gas_price()
     }
 
     pub async fn get_priority_fee(&self) -> Result<U256> {
-        self.node.lock().await.get_priority_fee()
+        self.node.write().await.get_priority_fee()
     }
 
     pub async fn get_block_number(&self) -> Result<u64> {
-        self.node.lock().await.get_block_number()
+        self.node.write().await.get_block_number()
     }
 
     pub async fn get_block_by_number(&self, block: &BlockTag) -> Result<ExecutionBlock> {
-        self.node.lock().await.get_block_by_number(block)
+        self.node.write().await.get_block_by_number(block)
     }
 
     pub async fn get_block_by_hash(&self, hash: &Vec<u8>) -> Result<ExecutionBlock> {
-        self.node.lock().await.get_block_by_hash(hash)
+        self.node.write().await.get_block_by_hash(hash)
     }
 
     pub async fn chain_id(&self) -> u64 {
-        self.node.lock().await.chain_id()
+        self.node.write().await.chain_id()
     }
 
     pub async fn get_header(&self) -> Header {
-        self.node.lock().await.get_header().clone()
+        self.node.write().await.get_header().clone()
     }
 }
