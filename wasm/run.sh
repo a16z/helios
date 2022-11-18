5f8e54ae12b80632b0fd8cb05cd41eb25d24f252
Noah Citron <noah@jeff.org>
2022-11-18 14:51:45 -0500

    proxy cors for testing

--- wasm/run.sh
+++ wasm/run.sh
@@ -1,1 +1,4 @@
+lcp --proxyUrl https://www.lightclientdata.org --port 9001 &
+lcp --proxyUrl https://eth-mainnet.g.alchemy.com/v2/23IavJytUwkTtBMpzt_TZKwgwAarocdT --port 9002 &
+
 CC=emcc AR=emar wasm-pack build --target web && simple-http-server --cors --nocache
