6b662f903bbb09c153a7b5a99b50d77a9d4afe00
Noah Citron <noah@jeff.org>
2023-01-31 18:23:55 -0500

    fix: benches (#184)

--- benches/file_db.rs
+++ benches/file_db.rs
@@ -1,4 +1,5 @@
 use client::database::Database;
+use config::Config;
 use criterion::{criterion_group, criterion_main, Criterion};
 use helios::prelude::FileDB;
 use tempfile::tempdir;
@@ -17,8 +18,12 @@ pub fn save_checkpoint(c: &mut Criterion) {
     c.bench_function("save_checkpoint", |b| {
         let checkpoint = vec![1, 2, 3];
         b.iter(|| {
-            let temp_dir = tempdir().unwrap().into_path();
-            let db = FileDB::new(temp_dir);
+            let data_dir = Some(tempdir().unwrap().into_path());
+            let config = Config {
+                data_dir,
+                ..Default::default()
+            };
+            let db = FileDB::new(&config).unwrap();
             db.save_checkpoint(checkpoint.clone()).unwrap();
         })
     });
@@ -28,14 +33,17 @@ pub fn save_checkpoint(c: &mut Criterion) {
 pub fn load_checkpoint(c: &mut Criterion) {
     c.bench_function("load_checkpoint", |b| {
         // First write to the db
-        let temp_dir = tempdir().unwrap().into_path();
-        let db = FileDB::new(temp_dir.clone());
-        let written_checkpoint = vec![1, 2, 3];
+        let data_dir = Some(tempdir().unwrap().into_path());
+        let config = Config {
+            data_dir,
+            ..Default::default()
+        };
+        let db = FileDB::new(&config).unwrap();
+        let written_checkpoint = vec![1; 32];
         db.save_checkpoint(written_checkpoint.clone()).unwrap();
 
         // Then read from the db
         b.iter(|| {
-            let db = FileDB::new(temp_dir.clone());
             let checkpoint = db.load_checkpoint().unwrap();
             assert_eq!(checkpoint, written_checkpoint.clone());
         })
