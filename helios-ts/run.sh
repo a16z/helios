5b18c3badad43e7cb9b6dbc827ed70bf7e46b5be
Noah Citron <noah@jeff.org>
2023-01-30 21:07:41 -0500

    remove ts bindings

--- helios-ts/run.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-(&>/dev/null lcp --proxyUrl https://www.lightclientdata.org --port 9001 &)
-(&>/dev/null lcp --proxyUrl https://eth-mainnet.g.alchemy.com/v2/23IavJytUwkTtBMpzt_TZKwgwAarocdT --port 9002 &)
-
-wasm-pack build
-npm run build
-simple-http-server
