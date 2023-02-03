0bcb3f8def7ed950674505841d73c24a62843968
danilowhk <danilowhk@gmail.com>
2023-02-03 16:30:48 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@
 //! Errors used across helios.
 
 pub mod client {
-    pub use client::{database::FileDB, Client, ClientBuilder};
+    pub use client::{database::{FileDB,ConfigDB}, Client, ClientBuilder};
 }
 
 pub mod config {
