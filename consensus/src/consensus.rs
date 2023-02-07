909c64be740e38f1764c7a6c4564863aefb1dffb
ckoopmann <c.k.e.koopmann@gmail.com>
2023-02-07 09:04:11 +0800

    Remove redundant get_block_from_rpc method

--- consensus/src/consensus.rs
+++ consensus/src/consensus.rs
@@ -82,7 +82,7 @@ impl<R: ConsensusRpc> ConsensusClient<R> {
 
     pub async fn get_execution_payload(&self, slot: &Option<u64>) -> Result<ExecutionPayload> {
         let slot = slot.unwrap_or(self.store.optimistic_header.slot);
-        let mut block = self.get_block_from_rpc(slot).await?;
+        let mut block = self.rpc.get_block(slot).await?;
         let block_hash = block.hash_tree_root()?;
 
         let latest_slot = self.store.optimistic_header.slot;
@@ -114,9 +114,10 @@ impl<R: ConsensusRpc> ConsensusClient<R> {
     ) -> Result<Vec<ExecutionPayload>> {
         let payloads_fut = (start_slot..end_slot)
             .rev()
-            .map(|slot| self.get_block_from_rpc(slot));
+            .map(|slot| self.rpc.get_block(slot));
         let mut prev_parent_hash: Bytes32 = self
-            .get_block_from_rpc(end_slot)
+            .rpc
+            .get_block(end_slot)
             .await?
             .body
             .execution_payload
@@ -141,10 +142,6 @@ impl<R: ConsensusRpc> ConsensusClient<R> {
             payloads.push(payload);
         }
         Ok(payloads)
-    }
-
-    pub async fn get_block_from_rpc(&self, slot: u64) -> Result<BeaconBlock> {
-        self.rpc.get_block(slot).await
     }
 
     pub fn get_header(&self) -> &Header {
