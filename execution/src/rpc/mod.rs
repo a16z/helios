623470ee599b26a6afffe165573d2c74ad23c936
SFYLL <santiagoflood@hotmail.fr>
2023-03-08 16:32:52 +0100

    module add get_fee_history

--- execution/src/rpc/mod.rs
+++ execution/src/rpc/mod.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use ethers::types::{
     transaction::eip2930::AccessList, Address, EIP1186ProofResponse, Filter, Log, Transaction,
-    TransactionReceipt, H256,
+    TransactionReceipt, H256, FeeHistory
 };
 use eyre::Result;
 
@@ -31,4 +31,5 @@ pub trait ExecutionRpc: Send + Clone + Sync + 'static {
     async fn get_transaction(&self, tx_hash: &H256) -> Result<Option<Transaction>>;
     async fn get_logs(&self, filter: &Filter) -> Result<Vec<Log>>;
     async fn chain_id(&self) -> Result<u64>;
+    async fn get_fee_history(&self, block_count: u64, last_block: u64, reward_percentiles: &[f64]) -> Result<FeeHistory>;
 }
