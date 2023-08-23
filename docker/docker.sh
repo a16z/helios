5554620dd066756654b8081783a30ad6dafa5de6
Luke Schoen <ltfschoen@gmail.com>
2023-08-23 15:48:31 +1000

    wip

--- docker/docker.sh
+++ docker/docker.sh
@@ -3,27 +3,61 @@
 trap "echo; exit" INT
 trap "echo; exit" HUP
 
-# install `realpath` on macOS from Homebrew `coreutils`
-# reference: https://unix.stackexchange.com/a/336138/269147
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  set -e
-  printf "\n*** Mac OS (Darwin) detected."
-  printf "\n*** Installation of Homebrew and coreutils is required by this script."
-  printf "\n*** Press SPACE to continue the installation or Ctrl+C to exit..."
-  while true; do
-    read -n1 -r
-    [[ $REPLY == ' ' ]] && break
-  done
-  printf "\n*** Continuing..."
+unknown_dist () {
+  printf "\n*** This OS is not supported with this script at present."
+  printf "\n*** Please try to manually install the 'realpath' binary and run the script again."
+  printf "\n*** Please also create an issue at https://github.com/a16z/helios/issues/new with details\n"
+  printf "\n*** of your OS that was missing the 'realpath' binary so we may improve this script."
 
-  if ! which brew >/dev/null 2>&1; then
-    printf "\n*** Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  exit 1;
+}
+
+if ! which realpath >/dev/null 2>&1; then
+  printf "\n*** Detected that the 'realpath' binary required by this script is not available"
+  # install `realpath` on macOS from Homebrew `coreutils`
+  # reference: https://unix.stackexchange.com/a/336138/269147
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    set -e
+    printf "\n*** Mac OS (Darwin) detected."
+    printf "\n*** Installation of Homebrew and coreutils."
+    printf "\n*** Press SPACE to continue the installation or Ctrl+C to exit..."
+    while true; do
+      read -n1 -r
+      [[ $REPLY == ' ' ]] && break
+    done
+    printf "\n*** Continuing..."
+
+    if ! which brew >/dev/null 2>&1; then
+      printf "\n*** Installing Homebrew..."
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    fi
+
+    printf "\n*** Updating Homebrew..."
+    # cannot run this as sudo user
+    brew update
+    printf "\n*** Installing coreutils..."
+    brew install coreutils
+  # credits: https://getsubstrate.io/
+  elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    set -e
+    if [[ $(whoami) == "root" ]]; then
+      MAKE_ME_ROOT=
+    else
+      MAKE_ME_ROOT=sudo
+    fi
+
+    if [ -f /etc/debian_version ]; then
+      echo "Ubuntu/Debian Linux detected."
+      $MAKE_ME_ROOT apt update
+      $MAKE_ME_ROOT apt install -y realpath
+    else
+      printf "\n*** Unknown Linux distribution."
+      unknown_dist
+    fi
+  else
+    printf "\n*** Unknown distribution."
+    unknown_dist
   fi
-
-  brew update
-  printf "\n*** Installing coreutils..."
-  brew install coreutils
 fi
 
 PARENT_DIR=$( echo $(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")") )
@@ -47,7 +81,7 @@ printf "\n*** Please wait... \n***"
 
 # https://stackoverflow.com/a/25554904/3208553
 set +e
-bash -e <<TRY
+bash -e << "TRY"
   docker build -f ${PARENT_DIR}/docker/Dockerfile -t helios:latest ./
 TRY
 if [ $? -ne 0 ]; then
