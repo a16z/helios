32f5a95223a97447b7e440d468749f9a2bf1bcde
Noah Citron <noah@jeff.org>
2024-03-11 18:10:19 -0400

    remove log

--- consensus/src/rpc/nimbus_rpc.rs
+++ consensus/src/rpc/nimbus_rpc.rs
@@ -41,8 +41,6 @@ impl ConsensusRpc for NimbusRpc {
             self.rpc, root_hex
         );
 
-        println!("{}", req);
-
         let res: BootstrapResponse = get(&req).await.map_err(|e| RpcError::new("bootstrap", e))?;
 
         Ok(res.data)
