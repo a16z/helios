960770b6126932d9c7a1e5c10c9e82662f9a0df4
Xavier DMello <xaviermdmello@gmail.com>
2024-07-18 14:23:07 -0700

    remove second bls lib

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -9,17 +9,6 @@ pub mod execution {
     pub mod types;
 }
 
-pub mod crypto {
-    #[cfg(feature = "milagro_bls")]
-    pub use milagro_bls as bls;
-
-    #[cfg(feature = "bls12_381")]
-    pub use types as bls;
-
-    pub mod consts;
-    pub mod types;
-}
-
 pub mod config {
     pub mod types;
     pub mod utils;
