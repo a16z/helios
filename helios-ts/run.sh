7283ca862bb2c35ee9313b8159685838fb0b89e5
Noah Citron <noah@jeff.org>
2024-10-07 17:46:24 -0400

    feat: wasm support for all networks (#384)
    * feat: wasm support for all networks
    
    * cleanup

--- helios-ts/run.sh
+++ helios-ts/run.sh
@@ -1,12 +1,3 @@
 set -e
-
-if [ -z $ETH_RPC_URL ]; then
-    echo "ETH_RPC_URL not set"
-    exit 1
-fi
-
-(&>/dev/null lcp --proxyUrl $ETH_RPC_URL --port 9001 &)
-(&>/dev/null lcp --proxyUrl https://www.lightclientdata.org --port 9002 &)
-
 npm run build
 simple-http-server
