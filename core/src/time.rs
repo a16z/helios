14ad4169a6e832e7e168955c09b331cd5cbe2925
Noah Citron <noah@jeff.org>
2024-10-10 12:25:11 -0400

    fix timing in wasm

---
+++ core/src/time.rs
@@ -0,0 +1,9 @@
+#[cfg(not(target_arch = "wasm32"))]
+pub use std::time::{SystemTime, UNIX_EPOCH};
+#[cfg(not(target_arch = "wasm32"))]
+pub use tokio::time::{interval, interval_at, Instant};
+#[cfg(target_arch = "wasm32")]
+pub use wasmtimer::{
+    std::{Instant, SystemTime, UNIX_EPOCH},
+    tokio::{interval, interval_at},
+};
