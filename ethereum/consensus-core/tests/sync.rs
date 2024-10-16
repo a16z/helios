6f36a9d38da00dd092e22ee6ae6106f5efc0df4c
Noah Citron <noah@jeff.org>
2024-10-16 13:21:47 -0700

    add other tests

--- ethereum/consensus-core/tests/sync.rs
+++ ethereum/consensus-core/tests/sync.rs
@@ -1,6 +1,16 @@
 mod runner;
 
 #[test]
-fn spec_sync_deneb() {
-    runner::run("testdata/deneb/sync");
+fn light_client_sync_deneb() {
+    runner::run("testdata/deneb/light_client_sync");
+}
+
+#[test]
+fn supply_sync_committee_from_past_update() {
+    runner::run("testdata/deneb/supply_sync_committee_from_past_update");
+}
+
+#[test]
+fn advance_finality_without_sync_committee() {
+    runner::run("testdata/deneb/advance_finality_without_sync_committee");
 }
