435cc200576b379aa76647a35358723b3c65f535
Noah Citron <noah@jeff.org>
2024-10-22 19:21:08 -0400

    feat: support server replicas

--- opstack/src/server/net/block_handler.rs
+++ opstack/src/server/net/block_handler.rs
@@ -1,6 +1,7 @@
 use alloy::primitives::Address;
 use libp2p::gossipsub::{IdentTopic, Message, MessageAcceptance, TopicHash};
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::mpsc::Sender;
+use tracing::info;
 
 use crate::SequencerCommitment;
 
@@ -12,16 +13,13 @@ pub struct BlockHandler {
 }
 
 impl BlockHandler {
-    pub fn new(signer: Address, chain_id: u64) -> (Self, Receiver<SequencerCommitment>) {
-        let (send, recv) = channel(256);
-        let handler = Self {
+    pub fn new(signer: Address, chain_id: u64, sender: Sender<SequencerCommitment>) -> Self {
+        Self {
             chain_id,
             signer,
-            commitment_sender: send,
+            commitment_sender: sender,
             blocks_v3_topic: IdentTopic::new(format!("/optimism/{}/2/blocks", chain_id)),
-        };
-
-        (handler, recv)
+        }
     }
 
     pub fn topics(&self) -> Vec<TopicHash> {
@@ -34,6 +32,7 @@ impl BlockHandler {
         };
 
         if commitment.verify(self.signer, self.chain_id).is_ok() {
+            info!("sending local commitment");
             _ = self.commitment_sender.try_send(commitment);
             MessageAcceptance::Accept
         } else {
