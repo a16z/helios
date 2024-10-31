664745efd9da2ad7f7f8a83e024c04f562d80559
Shourya Chaudhry <149008800+18aaddy@users.noreply.github.com>
2024-10-31 07:11:11 +0530

    fixes: correctly call get filter changes (#381)

--- core/src/client/mod.rs
+++ core/src/client/mod.rs
@@ -121,8 +121,8 @@ impl<N: NetworkSpec, C: Consensus<N::TransactionResponse>> Client<N, C> {
         self.node.get_logs(filter).await
     }
 
-    pub async fn get_filter_changes(&self, filter_id: U256) -> Result<bool> {
-        self.node.uninstall_filter(filter_id).await
+    pub async fn get_filter_changes(&self, filter_id: U256) -> Result<Vec<Log>> {
+        self.node.get_filter_changes(filter_id).await
     }
 
     pub async fn uninstall_filter(&self, filter_id: U256) -> Result<bool> {
