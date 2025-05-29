675d7616c9e1ee506e5e4648bbc0ce1281a77ee1
karen-sarkisyan <karen.sarkisyan01@gmail.com>
2025-05-29 22:20:15 +0200

    fix: vite config consistent formatting, conditional exports order

--- helios-ts/vite.config.js
+++ helios-ts/vite.config.js
@@ -1,14 +1,14 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 import { wasm } from "@rollup/plugin-wasm";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   mode: 'production',
   plugins: [
-    wasm({ 
+    wasm({
       targetEnv: 'auto-inline',
       maxFileSize: 100000000 // force inline wasm
     }),
