69233bb27d80822c26f5ba2c60d178a4f2109949
Karen Sarkisyan <karen.sarkisyan01@gmail.com>
2025-06-03 18:06:04 +0200

    feat: migrate to Vite, add ESM bundle (#616)
    * feat: migrate to Vite, add ESM bundle
    
    - Use vite for better DX and compatibility
    - Add ESM bundle
    - Node.js support for both UMD and ESM
    
    * fix: vite config consistent formatting, conditional exports order

---
+++ helios-ts/vite.config.js
@@ -0,0 +1,25 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import { wasm } from "@rollup/plugin-wasm";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  mode: 'production',
+  plugins: [
+    wasm({
+      targetEnv: 'auto-inline',
+      maxFileSize: 100000000 // force inline wasm
+    }),
+  ],
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, 'lib.ts'),
+      name: 'helios',
+      fileName: 'lib',
+      formats: ['umd', 'es'],
+    },
+  }
+})
