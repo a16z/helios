abf21fef86c0859c44b315b5a966af6b39de9dde
Noah Citron <noah@jeff.org>
2025-10-27 17:11:14 -0400

    fix clippy

--- helios-ts/src/opstack.rs
+++ helios-ts/src/opstack.rs
@@ -106,8 +106,8 @@ impl OpStackClient {
     }
 
     #[wasm_bindgen]
-    pub async fn wait_synced(&self) {
-        self.inner.wait_synced().await;
+    pub async fn wait_synced(&self) -> Result<(), JsError> {
+        map_err(self.inner.wait_synced().await)
     }
 
     #[wasm_bindgen]
