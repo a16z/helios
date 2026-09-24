e7332236cd39cade278a73b94015e46d50cd70dd
Noah Citron <noah@jeff.org>
2026-09-24 17:13:30 -0400

    fix: preserve full chain IDs in browser bindings

--- helios-ts/src/ethereum.rs
+++ helios-ts/src/ethereum.rs
@@ -174,8 +174,8 @@ impl EthereumClient {
     }
 
     #[wasm_bindgen]
-    pub fn chain_id(&self) -> u32 {
-        self.chain_id as u32
+    pub fn chain_id(&self) -> u64 {
+        self.chain_id
     }
 
     #[wasm_bindgen]
